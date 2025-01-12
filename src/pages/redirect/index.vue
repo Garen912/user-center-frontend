d1b32ff4835714031500c3242f20e106130bf784
Garen912 <936046325@qq.com>
2025-01-12 17:36:28 +0800

    fix: 解决 vue 缺少模板或渲染函数的警告问题

--- src/pages/redirect/index.vue
+++ src/pages/redirect/index.vue
@@ -1,8 +1,16 @@
+<template>
+  <div></div>
+</template>
+
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
 
+// 获取路由实例
 const router = useRouter()
-const { query } = router.currentRoute.value
-router.replace({ path: (query as { path: string }).path })
 
+// 获取当前路由的查询参数
+const { query } = router.currentRoute.value
+
+// 执行重定向
+router.replace({ path: (query as { path: string }).path })
 </script>
